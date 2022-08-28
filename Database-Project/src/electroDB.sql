4a440916bfa6e08937e6274373fd460943456c5a
Supermjork <markehabaziz@gmail.com>
2022-08-28 18:51:12 +0200

    Wrote more in the sql file

--- Database-Project/src/electroDB.sql
+++ Database-Project/src/electroDB.sql
@@ -1,17 +1,17 @@
-CREATE TABLE seller(seller_id SERIAL PRIMARY KEY,
-                    seller_name VARCHAR(255),
-                    seller_password VARCHAR(255));
+CREATE TABLE seller(seller_id INTEGER PRIMARY KEY,
+                    seller_name VARCHAR(255) NOT NULL,
+                    seller_password VARCHAR(255) NOT NULL);
 
-CREATE TABLE systemAdmin(system_id INT PRIMARY KEY,
-                         adminName VARCHAR(255),
-                         admin_Password VARCHAR(255));
+CREATE TABLE systemAdmin(system_id INTEGER PRIMARY KEY,
+                         adminName VARCHAR(255) NOT NULL,
+                         admin_Password VARCHAR(255) NOT NULL);
 
-CREATE TABLE customer(user_id SERIAL PRIMARY KEY,
-                      username VARCHAR(255),
-                      customer_password VARCHAR(255),
-                      address_city VARCHAR(255),
-                      address_district VARCHAR(255),
-                      address_zipcode VARCHAR(255));
+CREATE TABLE customer(user_id INTEGER PRIMARY KEY,
+                      username VARCHAR(255) NOT NULL ,
+                      customer_password VARCHAR(255) NOT NULL ,
+                      address_city VARCHAR(255) NOT NULL ,
+                      address_district VARCHAR(255) NOT NULL ,
+                      address_zipcode VARCHAR(255) NOT NULL );
 
 CREATE TABLE handheld(inventory_id VARCHAR(255) PRIMARY KEY,
                       brand VARCHAR(255) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE computer(inventory_id VARCHAR(255) PRIMARY KEY,
                       price NUMERIC(10, 2) NOT NULL,
                       amountinstock INTEGER NOT NULL,
                       cpu VARCHAR(255) NOT NULL,
-                      gpu VARCHAR(255) NOT NULL,
+                      gpu VARCHAR(255),
                       operatingsys VARCHAR(255) NOT NULL,
                       ram INTEGER NOT NULL,
                       devicestorage INTEGER NOT NULL,
@@ -47,14 +47,28 @@ CREATE TABLE computer(inventory_id VARCHAR(255) PRIMARY KEY,
 
 CREATE TABLE lists(seller_id INTEGER NOT NULL,
                    inventory_id VARCHAR(255) NOT NULL,
-                   admin_id INTEGER NOT NULL);
+                   admin_id INTEGER NOT NULL,
+                   CONSTRAINT fk1 FOREIGN KEY (seller_id) REFERENCES seller(seller_id),
+                   CONSTRAINT fk2_1 FOREIGN KEY (inventory_id) REFERENCES handheld(inventory_id),
+                   CONSTRAINT fk2_2 FOREIGN KEY (inventory_id) REFERENCES computer(inventory_id),
+                   CONSTRAINT fk2_3 FOREIGN KEY (inventory_id) REFERENCES camera(inventory_id),
+                   CONSTRAINT pk PRIMARY KEY(seller_id, inventory_id));
 
 --Need to split the multivalued into other tables
 CREATE TABLE orders(order_id SERIAL PRIMARY KEY,
                     customer_id INTEGER NOT NULL,
-                    seller_id INTEGER NOT NULL,
-                    device_id VARCHAR(255) NOT NULL,
-                    order_date DATE NOT NULL);
+                    order_date DATE NOT NULL,
+                    CONSTRAINT fk FOREIGN KEY (customer_id) REFERENCES customer(user_id));
+
+CREATE TABLE orders1(order_id INTEGER NOT NULL,
+                     seller_id INTEGER NOT NULL,
+                     device_id VARCHAR(255) NOT NULL,
+                     CONSTRAINT fk1 FOREIGN KEY (order_id) REFERENCES orders(order_id),
+                     CONSTRAINT fk2 FOREIGN KEY (seller_id) REFERENCES seller(seller_id),
+                     CONSTRAINT fk3_1 FOREIGN KEY (device_id) REFERENCES handheld(inventory_id),
+                     CONSTRAINT fk3_2 FOREIGN KEY (device_id) REFERENCES computer(inventory_id),
+                     CONSTRAINT fk3_3 FOREIGN KEY (device_id) REFERENCES camera(inventory_id),
+                     CONSTRAINT pk PRIMARY KEY(order_id, seller_id, device_id));
 
 CREATE TABLE issueorder(order_id INTEGER NOT NULL,
                         inventory_id VARCHAR(255) NOT NULL,
