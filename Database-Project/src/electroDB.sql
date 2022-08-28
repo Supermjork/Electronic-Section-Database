3560031c77d03fc825f88bee3837c9f1fbf72287
Supermjork <markehabaziz@gmail.com>
2022-08-28 20:00:32 +0200

    Continuted writing in sql file, maybe solved problems?

--- Database-Project/src/electroDB.sql
+++ Database-Project/src/electroDB.sql
@@ -13,12 +13,13 @@ CREATE TABLE customer(user_id INTEGER PRIMARY KEY,
                       address_district VARCHAR(255) NOT NULL ,
                       address_zipcode VARCHAR(255) NOT NULL );
 
+--All devices will have their own unique ID, instead of ID being for the batch of same device
+--Displaying the stock amount can just use the COUNT function, going over the device name
 CREATE TABLE handheld(inventory_id VARCHAR(255) PRIMARY KEY,
                       brand VARCHAR(255) NOT NULL,
                       manufacturer VARCHAR(255) NOT NULL,
                       reviewrating NUMERIC(2, 1) NOT NULL,
                       price NUMERIC(10, 2) NOT NULL,
-                      amountinstock INTEGER NOT NULL,
                       devicetype VARCHAR(255) NOT NULL,
                       screensize NUMERIC(5,2) NOT NULL,
                       devicestorage INTEGER NOT NULL);
@@ -28,7 +29,6 @@ CREATE TABLE camera(inventory_id VARCHAR(255) PRIMARY KEY,
                     manufacturer VARCHAR(255) NOT NULL,
                     reviewRating NUMERIC(2, 1) NOT NULL,
                     price NUMERIC(10, 2) NOT NULL,
-                    amountinstock INTEGER NOT NULL,
                     focallength NUMERIC(4,2) NOT NULL,
                     cameratype VARCHAR(255) NOT NULL);
 
@@ -37,7 +37,6 @@ CREATE TABLE computer(inventory_id VARCHAR(255) PRIMARY KEY,
                       manufacturer VARCHAR(255) NOT NULL,
                       reviewrating NUMERIC(2, 1) NOT NULL,
                       price NUMERIC(10, 2) NOT NULL,
-                      amountinstock INTEGER NOT NULL,
                       cpu VARCHAR(255) NOT NULL,
                       gpu VARCHAR(255),
                       operatingsys VARCHAR(255) NOT NULL,
@@ -54,23 +53,30 @@ CREATE TABLE lists(seller_id INTEGER NOT NULL,
                    CONSTRAINT fk2_3 FOREIGN KEY (inventory_id) REFERENCES camera(inventory_id),
                    CONSTRAINT pk PRIMARY KEY(seller_id, inventory_id));
 
---Need to split the multivalued into other tables
+/*Essentially wrapping the issueOrder data and having them refer to the same orderID in
+  a separate table
+  Need to split the multivalued into other tables [Done]
+ */
 CREATE TABLE orders(order_id SERIAL PRIMARY KEY,
                     customer_id INTEGER NOT NULL,
                     order_date DATE NOT NULL,
-                    CONSTRAINT fk FOREIGN KEY (customer_id) REFERENCES customer(user_id));
+                    CONSTRAINT fk FOREIGN KEY (customer_id) REFERENCES issueorder(customer_id));
 
 CREATE TABLE orders1(order_id INTEGER NOT NULL,
                      seller_id INTEGER NOT NULL,
                      device_id VARCHAR(255) NOT NULL,
                      CONSTRAINT fk1 FOREIGN KEY (order_id) REFERENCES orders(order_id),
-                     CONSTRAINT fk2 FOREIGN KEY (seller_id) REFERENCES seller(seller_id),
-                     CONSTRAINT fk3_1 FOREIGN KEY (device_id) REFERENCES handheld(inventory_id),
-                     CONSTRAINT fk3_2 FOREIGN KEY (device_id) REFERENCES computer(inventory_id),
-                     CONSTRAINT fk3_3 FOREIGN KEY (device_id) REFERENCES camera(inventory_id),
-                     CONSTRAINT pk PRIMARY KEY(order_id, seller_id, device_id));
+                     CONSTRAINT fk2 FOREIGN KEY (seller_id) REFERENCES issueorder(seller_id),
+                     CONSTRAINT fk3 FOREIGN KEY (device_id) REFERENCES issueorder(inventory_id),
+                     CONSTRAINT pk PRIMARY KEY(order_id, device_id));
 
-CREATE TABLE issueorder(order_id INTEGER NOT NULL,
-                        inventory_id VARCHAR(255) NOT NULL,
+--Issued orders get put into orders1, under the same serialised order_ID
+--seller_id can be traced through the item's ID in the "lists" table
+CREATE TABLE issueorder(inventory_id VARCHAR(255) NOT NULL,
                         seller_id INTEGER NOT NULL,
-                        customer_id INTEGER);
+                        customer_id INTEGER,
+                        CONSTRAINT fk1_1 FOREIGN KEY (inventory_id) REFERENCES handheld(inventory_id),
+                        CONSTRAINT fk1_2 FOREIGN KEY (inventory_id) REFERENCES computer(inventory_id),
+                        CONSTRAINT fk1_3 FOREIGN KEY (inventory_id) REFERENCES camera(inventory_id),
+                        CONSTRAINT fk2 FOREIGN KEY (seller_id) REFERENCES lists(seller_id),
+                        CONSTRAINT fk3 FOREIGN KEY (customer_id) REFERENCES customer(user_id));
